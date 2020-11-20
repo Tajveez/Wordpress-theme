09726c69df52f0266edb2b1394aada716b373d54
Tajveez <Tajveez786@gmail.com>
2020-11-20 23:26:01 +0500

    Widgets and sidebar added

--- functions.php
+++ functions.php
@@ -37,5 +37,21 @@ add_theme_support('custom-background');
 add_theme_support('custom-header');
 add_theme_support('post-thumbnails');
 
-// Not working...
 add_theme_support('post-formats', ['aside', 'image']);
+
+// sidebar
+function twentytwentyone_widget_setup() {
+    register_sidebar(
+        array(
+            'name' => 'Sidebar',
+            'id' => 'sidebar-1',
+            'class' => 'custom',
+            'description' => 'standard sidebar',
+            'before_widget'=>'<aside id="%1$s" class="widget %2$s">',
+            'after_widget'=>'</aside>',
+            'before_title' => '<h1 class="widget-title">',
+            'after_title'=>'</h1>'
+        )
+    );
+}
+add_action('widgets_init', 'twentytwentyone_widget_setup');
