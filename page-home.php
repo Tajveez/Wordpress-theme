ed8f711469b0e001501a813fdeeeee319a52b04d
Tajveez <Tajveez786@gmail.com>
2020-11-22 23:33:11 +0500

    WP_Query part 2

--- page-home.php
+++ page-home.php
@@ -2,7 +2,11 @@
 <div class="row">
     <div class="col-xs-12">
         <?php
-        $lastBlog = new WP_Query('type=post&posts_per_page=1');
+        $args = array(
+            'type' => 'post',
+            'posts_per_page' => 1
+        );
+        $lastBlog = new WP_Query($args);
         if ($lastBlog->have_posts()) :
             while ($lastBlog->have_posts()) : $lastBlog->the_post();
                 get_template_part('content', get_post_format());
@@ -20,7 +24,27 @@
                 <?php get_template_part('content', get_post_format()); ?>
         <?php
             endwhile;
-        endif; ?>
+        endif;
+
+        $lastBlog = new WP_Query('type=post&posts_per_page=2&offset=1');
+        if ($lastBlog->have_posts()) :
+            while ($lastBlog->have_posts()) : $lastBlog->the_post();
+                get_template_part('content', get_post_format());
+            endwhile;
+        endif;
+        wp_reset_postdata();
+        ?>
+        <hr>
+        <?php
+
+        $lastBlog = new WP_Query('type=post&posts_per_page=-1&cat=5');
+        if ($lastBlog->have_posts()) :
+            while ($lastBlog->have_posts()) : $lastBlog->the_post();
+                get_template_part('content', get_post_format());
+            endwhile;
+        endif;
+        wp_reset_postdata();
+        ?>
     </div>
     <div class="col-xs-12 col-sm-4">
         <?php get_sidebar() ?>
