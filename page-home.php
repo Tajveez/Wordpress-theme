7ae1eac4c4d9408acc1ee9bb019ff5d24ff2da4d
Tajveez <Tajveez786@gmail.com>
2020-11-25 23:49:55 +0500

    More WP_Query

--- page-home.php
+++ page-home.php
@@ -1,19 +1,28 @@
 <?php get_header() ?>
 <div class="row">
     <?php
-    $args = array(
-        'type' => 'post',
-        'posts_per_page' => 3
+    $args_cat = array(
+        'include' => '1, 5, 7'
     );
-    $lastBlog = new WP_Query($args);
-    if ($lastBlog->have_posts()) :
-        while ($lastBlog->have_posts()) : $lastBlog->the_post(); ?>
-            <div class="col-xs-12 col-sm-4">
-                <?php get_template_part('content', 'featured'); ?>
-            </div>
+    $categories = get_categories($args_cat);
+    foreach ($categories as $category) :
+        # code...
+        $args = array(
+            'type' => 'post',
+            'posts_per_page' => 1,
+            'category__in' => $category->term_id,
+            'category__not_in' => array(6)
+        );
+        $lastBlog = new WP_Query($args);
+        if ($lastBlog->have_posts()) :
+            while ($lastBlog->have_posts()) : $lastBlog->the_post(); ?>
+                <div class="col-xs-12 col-sm-4">
+                    <?php get_template_part('content', 'featured'); ?>
+                </div>
     <?php
-        endwhile;
-    endif;
+            endwhile;
+        endif;
+    endforeach;
     wp_reset_postdata();
     ?>
 </div>
