884090e810f9de57fd1cde2fb54bddd03024e163
Tajveez <Tajveez786@gmail.com>
2020-12-05 23:05:16 +0500

    Submenu editing

--- inc/walker.php
+++ inc/walker.php
@@ -1,3 +1,26 @@
 <?php
 
 /* Collection of Walker Classes */
+class Walker_Nav_Primary extends Walker_Nav_Menu
+{
+    function start_lvl(&$output, $depth = 0, $args = null)
+    {
+        $indent = str_repeat("\t", $depth);
+        $submenu = ($depth > 0) ? 'sub-menu' : '';
+        $output .= "\n$indent<ul class=\"dropdown-menu$submenu depth_$depth\">\n";
+    }
+
+    function start_el(&$output, $item, $depth = 0, $args = null, $id = 0)
+    {
+    }
+
+    /*
+    function end_el(&$output, $item, $depth = 0, $args = null)
+    {
+    }
+
+    function end_lvl(&$output, $depth = 0, $args = null)
+    {
+    } 
+    */
+}
